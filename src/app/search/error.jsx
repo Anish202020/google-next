4c28785c98be6d6e0d106d6d606b07acb1cacb6b
ANISH KUMAR <anishkumarbdmi@gmail.com>
2024-08-30 11:24:57 +0530

    commit changes

--- src/app/search/error.jsx
+++ src/app/search/error.jsx
@@ -2,14 +2,22 @@
 
 import { useEffect } from 'react';
 
-export default function Error({ error, reset }) {
+type ErrorProps = {
+  error: Error; // Ensure error is of type Error
+  reset: () => void; // Function to reset the error state
+};
+
+export default function Error({ error, reset }: ErrorProps) {
   useEffect(() => {
-    console.log('error', error);
+    console.error('Error:', error);
   }, [error]);
+
   return (
     <div className='flex flex-col justify-center items-center pt-10'>
       <h1 className='text-3xl mb-4'>Something went wrong!</h1>
-      <button className='text-blue-500'>Try again</button>
+      <button onClick={reset} className='text-blue-500'>
+        Try again
+      </button>
     </div>
   );
 }
